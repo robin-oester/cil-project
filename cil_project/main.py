2e9b59b0f0654faeb291ab10c3f846cc5462113c
Robin Oester <robin.oester@gmail.com>
2024-06-17 16:46:48 +0200

    Follow-Up * fix main.py

--- cil_project/main.py
+++ cil_project/main.py
@@ -1,17 +1,9 @@
 import pathlib
 
-from dataset import BalancedKFold, RatingsDataset
+from dataset import RatingsDataset
 
 if __name__ == "__main__":
 
     # Example usage
-    dataset = RatingsDataset(file_path=pathlib.Path("../data/data_train.csv"))
+    dataset = RatingsDataset.deserialize(pathlib.Path("../data/serialized_ratings.npz"))
     print(dataset[0])
-
-    k_fold = BalancedKFold(5, True)
-    iterator = k_fold.split(dataset)
-
-    for train_idx, test_idx in iterator:
-        training_samples = len(train_idx)
-        test_samples = len(test_idx)
-        print(training_samples + test_samples)
