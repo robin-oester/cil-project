01891302b681d6ef77cd2f71de5a5a3e8dc2f8e1
pineri <piero.neri99@bluewin.ch>
2024-07-05 18:02:23 +0200

    Add code to store k-fold cross-validation results (This might be temporary code)

--- cil_project/baseline_models/models/svt.py
+++ cil_project/baseline_models/models/svt.py
@@ -13,6 +13,7 @@ class SVT(Baseline, RatingPredictor):
         self.verbose = verbose
         self.test_m = np.array([])
         self.test_m_mask = np.array([])
+        self.rmse = 0.0
         # Define hyperparameters dictionary
         self.hyperparameters = {"max_iter": max_iter, "eta": eta, "tau": tau}
 
@@ -42,7 +43,9 @@ class SVT(Baseline, RatingPredictor):
             ]
             if self.verbose and not (self.test_m.size == 0 and self.test_m_mask.size == 0):
                 r = np.clip(self.reconstructed_matrix.copy() * self.column_std + self.column_mean, 1, 5)
-                print(f"Iteration {it+1}, Validation RMSE: {masked_rmse(self.test_m, r, self.test_m_mask)}")
+                rmse = masked_rmse(self.test_m, r, self.test_m_mask)
+                self.rmse = rmse
+                print(f"Iteration {it+1}, Validation RMSE: {rmse}")
 
     def train(
         self, data_matrix: np.ndarray, test_m: np.ndarray = np.array([]), test_m_mask: np.ndarray = np.array([])
--- cil_project/baseline_models/trainer_and_evaluator/svt_trainer_and_evaluator.py
+++ cil_project/baseline_models/trainer_and_evaluator/svt_trainer_and_evaluator.py
@@ -1,6 +1,19 @@
+import os
+
+import numpy as np
+import torch
 from cil_project.baseline_models.models.svt import SVT
 from cil_project.dataset import BalancedKFold, RatingsDataset
 from cil_project.utils import FULL_SERIALIZED_DATASET_NAME, SUBMISSION_FILE_NAME
+
+STORE_KFOLD = False
+if STORE_KFOLD:
+    np.random.seed(0)
+    torch.manual_seed(0)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(0)
+        torch.cuda.manual_seed_all(0)
+
 
 ONLY_GENERATE_SUBMISSION = False
 VERBOSE = True
@@ -16,6 +29,8 @@ if __name__ == "__main__":
     # prepare dataset
     dataset = RatingsDataset.load(FULL_SERIALIZED_DATASET_NAME)
     kfold = BalancedKFold(num_folds=10, shuffle=True)
+
+    avg_rmse = 0.0  # pylint: disable=invalid-name
 
     # initialize SVT model
     svt = SVT(max_iter=MAX_ITER, eta=ETA, tau=TAU, verbose=VERBOSE)
@@ -35,6 +50,16 @@ if __name__ == "__main__":
 
         # train model
         svt.train(train_matrix, test_matrix, test_matrix_mask)
+        avg_rmse += svt.rmse
+
+        if STORE_KFOLD:
+            print("length of test dataset: ", len(test_data))
+            preds = svt.predict(test_data.get_inputs())
+            file_path = os.path.join(os.path.dirname(__file__), "svt_kfold_results.txt")
+            with open(file_path, "ab") as f:
+                np.savetxt(f, preds, fmt="%f")
+
+    print(f"Average RMSE over 10 folds: {avg_rmse / 10.0}")
 
     # train on the whole dataset and generate submission file
     print("Training on the whole dataset")
