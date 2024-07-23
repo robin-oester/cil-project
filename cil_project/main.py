e1737164db408a6e3466cd71a46b41b8ae439af3
Lukas Nüesch <lnueesch@student.ethz.ch>
2024-07-23 14:54:14 +0200

    Revert main.py to origin/main

--- cil_project/main.py
+++ cil_project/main.py
@@ -9,40 +9,10 @@ if __name__ == "__main__":
     # Example usage
     model = NCFCombined.load_from_checkpoint("NCFCombined_5_2024-06-25_10:02:46.pkl")
     dataset = RatingsDataset.load(FULL_SERIALIZED_DATASET_NAME)
-    # test bin sizes from 2 to 1024 in powers of 2
-    for i in range(1, 9):
-        NUM_BINS = 2**i
-        NUM_CLUSTERS = 2**i
-        print(f"Training with {NUM_BINS} bins")
-        # print(f"Training with {NUM_CLUSTERS} clusters")
-        trainer = BFMTrainingProcedure(
-            rank=15,
-            num_bins=NUM_BINS,
-            num_clusters=NUM_CLUSTERS,
-            iterations=1000,
-            kfold=10,
-            dataset=dataset,
-            grouped=False,
-            implicit=False,
-            statistical_features=True,
-            ordinal_probit=False,
-            kmeans=False,
-        )
-        trainer.start_training()
 
-    # for final train
-    # trainer = BFMTrainingProcedure(
-    #     rank=2,
-    #     num_bins=10,
-    #     iterations=100,
-    #     kfold=10,
-    #     dataset=dataset,
-    #     grouped=True,
-    #     implicit=True,
-    #     statistical_features=False,
-    #     ordinal_probit=False,
-    #     kmeans=False,
-    # )
-    # trainer.final_train()
+    # optional normalization
+    # dataset.normalize(TargetNormalization.BY_MOVIE)
 
-    # trainer.model.generate_predictions(SUBMISSION_FILE_NAME)
+    evaluator = RatingEvaluator(model, 64, dataset, None)
+
+    evaluator.generate_predictions(SUBMISSION_FILE_NAME)
